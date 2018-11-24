dc79cbb62a2505c555f6a6aecfa210c98cb816ba
Jose Herrera <31715468+joseherrera7@users.noreply.github.com>
2018-11-24 01:00:54 -0600

    siii

--- app/src/main/java/com/example/godin/proyecto_ed2/chat.java
+++ app/src/main/java/com/example/godin/proyecto_ed2/chat.java
@@ -1,6 +1,11 @@
 package com.example.godin.proyecto_ed2;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.provider.MediaStore.MediaColumns;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -8,7 +13,8 @@ import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 import android.widget.Toolbar;
-import org.apache.http.*;
+
+
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -17,10 +23,28 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.apache.http.entity.mime.HttpMultipartMode;
+import org.apache.http.entity.mime.MultipartEntity;
+import org.apache.http.entity.mime.content.FileBody;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
@@ -36,26 +60,41 @@ public class chat extends AppCompatActivity {
     zigzag z=new zigzag();
     Toolbar toolbar;
     RequestQueue queue;
- int clavecifrado;
+    int clavecifrado;
     EditText messagetext;
-   private  String id;
- private ArrayList<mensaje> messages;
+    Button atach, act , env;s
+    private  String id;
+    ArrayList<mensaje> messages;
+    String filePath, file_extn;
 
+    private String readTextFromUri(Uri uri) throws IOException {
+        String salida="";
+
+        InputStream inputStream = getContentResolver().openInputStream(uri);
+        String cadena="";
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream,"utf8"),8192);
+        while((cadena = reader.readLine())!=null) {
+            salida=salida+cadena+"\n";
+        }
+        inputStream.close();
+        reader.close();
+        return salida;
+    }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
+        queue= Volley.newRequestQueue(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
-        Button act=(Button) findViewById(R.id.button3);
-        Button env=(Button) findViewById(R.id.send);
-        Button atach =(Button) findViewById(R.id.atach);
-         messagetext =(EditText) findViewById(R.id.message);
+        act=(Button) findViewById(R.id.button3);
+        env=(Button) findViewById(R.id.send);
+        atach =(Button) findViewById(R.id.atach);
+        messagetext =(EditText) findViewById(R.id.message);
         usuariologeado=getIntent().getExtras().getString("usuario");
         elegido=getIntent().getExtras().getString("usuarioelegido");
         token=getIntent().getExtras().getString("token");
-        queue= Volley.newRequestQueue(this);
+
 
         mensjesmostrados =(ListView) findViewById(R.id.mensajesenpantalla);
 
@@ -86,6 +125,12 @@ public class chat extends AppCompatActivity {
         atach.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                Intent intento = new Intent()
+                        .addCategory(Intent.CATEGORY_OPENABLE)
+                        .setType("*/*")
+                        .setAction(Intent.ACTION_OPEN_DOCUMENT);
+
+                startActivityForResult(Intent.createChooser(intento, "Seleccione un archivo"), 123);
 
             }
         });
@@ -96,7 +141,23 @@ public class chat extends AppCompatActivity {
 
 
 
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 123 && resultCode == RESULT_OK){
+            Uri selectedImage = data.getData();
 
+             filePath = null;
+            try {
+                filePath = readTextFromUri(selectedImage);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            file_extn = filePath.substring(filePath.lastIndexOf(".") + 1);
+            atach.setText(filePath);
+
+
+        }
+    }
     public void onResume() {
         super.onResume();
 
@@ -140,7 +201,7 @@ public class chat extends AppCompatActivity {
             @Override
             public void onResponse(JSONObject response) {
 
-             Toast.makeText(chat.this,"Su mensaje fue enviado con exito",Toast.LENGTH_SHORT ).show();
+                Toast.makeText(chat.this,"Su mensaje fue enviado con exito",Toast.LENGTH_SHORT ).show();
             }
         }, new Response.ErrorListener() {
             @Override
@@ -169,68 +230,68 @@ public class chat extends AppCompatActivity {
 
     }
 
-       public void obtenerdatos(final VolleyCallback<ArrayList<mensaje>> callback){
+    public void obtenerdatos(final VolleyCallback<ArrayList<mensaje>> callback){
 
-           String url=Login.url+"/chat";
+        String url=Login.url+"/chat";
 
-           queue.add(new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-               @Override
-               public void onResponse(JSONObject response) {
-                   try {
-                       ArrayList<mensaje> lisa=new ArrayList<>();
-                       JSONObject conversacion;
-                       JSONArray array =response.getJSONArray("chats");
+        queue.add(new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    ArrayList<mensaje> lisa=new ArrayList<>();
+                    JSONObject conversacion;
+                    JSONArray array =response.getJSONArray("chats");
 
-                       for (int i=0;i<array.length();i++){
+                    for (int i=0;i<array.length();i++){
 
-                           conversacion=array.getJSONObject(i);
-                           if(conversacion.getString("user1").equals(usuariologeado)){
-                               if(conversacion.getString("user2").equals(elegido)){
+                        conversacion=array.getJSONObject(i);
+                        if(conversacion.getString("user1").equals(usuariologeado)){
+                            if(conversacion.getString("user2").equals(elegido)){
 
-                                   lisa=JSONARRAYTOARRAY(conversacion.getJSONArray("messages"));
-                                   callback.onSuccess(lisa);
-                               }
+                                lisa=JSONARRAYTOARRAY(conversacion.getJSONArray("messages"));
+                                callback.onSuccess(lisa);
+                            }
 
-                           }else if(conversacion.getString("user2").equals(usuariologeado)){
-                               if(conversacion.getString("user1").equals(elegido)){
+                        }else if(conversacion.getString("user2").equals(usuariologeado)){
+                            if(conversacion.getString("user1").equals(elegido)){
 
-                                   lisa=JSONARRAYTOARRAY(conversacion.getJSONArray("messages"));
-                                   callback.onSuccess(lisa);
-                               }
+                                lisa=JSONARRAYTOARRAY(conversacion.getJSONArray("messages"));
+                                callback.onSuccess(lisa);
+                            }
 
-                           }
+                        }
 
 
-                       }
-                   } catch (JSONException e) {
-                       e.printStackTrace();
-                   }
-               }
-           }, new Response.ErrorListener() {
-               @Override
-               public void onErrorResponse(VolleyError error) {
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
 
-               }
-           }){
+            }
+        }){
 
-               //This is for Headers If You Needed
-               @Override
-               public Map<String, String> getHeaders() throws AuthFailureError {
-                   Map<String, String> params = new HashMap<String, String>();
-                   params.put("Content-Type", "application/json; charset=UTF-8");
-                   params.put("Authorization", token);
-                   return params;
-               }
+            //This is for Headers If You Needed
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Content-Type", "application/json; charset=UTF-8");
+                params.put("Authorization", token);
+                return params;
+            }
 
-               //Pass Your Parameters here
-               @Override
-               protected Map<String, String> getParams() {
-                   Map<String, String> params = new HashMap<String, String>();
-                   params.put("user", usuariologeado);
+            //Pass Your Parameters here
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("user", usuariologeado);
 
-                   return params;
-               } });
-       }
+                return params;
+            } });
+    }
 
 
     public void obteneridconversacion(final VolleyCallback callback){
@@ -250,8 +311,8 @@ public class chat extends AppCompatActivity {
                         if(conversacion.getString("user1").equals(usuariologeado)){
                             if(conversacion.getString("user2").equals(elegido)){
 
-                              callback.onSuccess(conversacion.getString("_id"));
-                              break;
+                                callback.onSuccess(conversacion.getString("_id"));
+                                break;
                             }
 
                         }else if(conversacion.getString("user2").equals(usuariologeado)){
@@ -317,7 +378,7 @@ public class chat extends AppCompatActivity {
                         }else if(conversacion.getString("user2").equals(usuariologeado)){
                             if(conversacion.getString("user1").equals(elegido)){
 
-                              //  mensajes=JSONARRAYTOARRAY(array);
+                                //  mensajes=JSONARRAYTOARRAY(array);
                             }
 
                         }
@@ -334,48 +395,24 @@ public class chat extends AppCompatActivity {
         });
     }
 
-    public void uploadatach(){
+    public void uploadatach() throws IOException {
 
-        String url="peticion de conversaciones";
+        String url= Login.url + "/chat/upload";
 
-        JsonObjectRequest request=new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    JSONObject conversacion;
-                    JSONArray array =response.getJSONArray("");
+        HttpClient httpclient = new DefaultHttpClient();
+        HttpPost httppost = new HttpPost(url);
+        MultipartEntity mpEntity = new MultipartEntity(HttpMultipartMode.BROWSER_COMPATIBLE);
+        if (filePath != null) {
+            File file = new File(filePath);
 
-                    for (int i=0;i<array.length();i++){
-
-                        conversacion=array.getJSONObject(i);
-                        if(conversacion.getString("user1").equals(usuariologeado)){
-                            if(conversacion.getString("user2").equals(elegido)){
-
-                                //mensajes=JSONARRAYTOARRAY(array);
-                            }
-
-                        }else if(conversacion.getString("user2").equals(usuariologeado)){
-                            if(conversacion.getString("user1").equals(elegido)){
-
-                               // mensajes=JSONARRAYTOARRAY(array);
-                            }
-
-                        }
-                    }
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-
-            }
-        });
+            mpEntity.addPart("avatar", new FileBody(file, "application/octet"));
+        }
+        httppost.setEntity(mpEntity);
+        HttpResponse response = httpclient.execute(httppost);
     }
     public  ArrayList<mensaje>  JSONARRAYTOARRAY(JSONArray jarray) throws JSONException {
-    clavecifrado=usuariologeado.length()+elegido.length();
-         ArrayList<mensaje> mensajes=new ArrayList();
+        clavecifrado=usuariologeado.length()+elegido.length();
+        ArrayList<mensaje> mensajes=new ArrayList();
         for(int x=0;x<jarray.length();x++){
             JSONObject mensajedeljson=jarray.getJSONObject(x);
             mensaje mensajeparainsertar=new mensaje();
@@ -395,7 +432,7 @@ public class chat extends AppCompatActivity {
         JSONArray mensajes=new JSONArray();
         for(int x=0;x<lisa.size();x++){
             JSONObject jsonbod=mensajes.getJSONObject(x);
-           mensaje message=lisa.get(x);
+            mensaje message=lisa.get(x);
             mensaje mensajeparainsertar=new mensaje();
             jsonbod.put("emisor",mensajeparainsertar.emisor);
             jsonbod.put("mensaje",mensajeparainsertar.mensaje);
